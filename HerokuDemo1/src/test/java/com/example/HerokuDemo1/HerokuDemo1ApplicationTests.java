0327ea584914ede930db9519032925fe006ab7f3
Mihkel Tiganik <mtiganik@gmail.com>
2023-09-09 23:34:15 +0300

    commit

---
+++ HerokuDemo1/src/test/java/com/example/HerokuDemo1/HerokuDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HerokuDemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokuDemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
