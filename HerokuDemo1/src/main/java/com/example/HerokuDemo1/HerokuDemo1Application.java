9ee6790e6aa6608d5c52dec224344d5039751408
Mihkel Tiganik <mtiganik@gmail.com>
2023-09-09 23:49:46 +0300

    commit

--- HerokuDemo1/src/main/java/com/example/HerokuDemo1/HerokuDemo1Application.java
+++ HerokuDemo1/src/main/java/com/example/HerokuDemo1/HerokuDemo1Application.java
@@ -2,9 +2,19 @@ package com.example.HerokuDemo1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class HerokuDemo1Application {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HerokuDemo1Application.class, args);
